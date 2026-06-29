9b180ddd9c9d79d94dbfbb6cca2864f0206abcdf
Tejas <jaybalaji192@gmail.com>
2026-06-29 13:59:11 +0530

    fix: remove base path configuration from Vite setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,6 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-    base: '/snipit/',
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
