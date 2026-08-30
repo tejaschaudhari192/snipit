57693e9d9a7a595a02fbe92b5ab18024f7845c78
Tejas <jaybalaji192@gmail.com>
2026-08-30 12:27:41 +0530

    misc: select update

--- frontend/src/components/ui/select.tsx
+++ frontend/src/components/ui/select.tsx
@@ -87,7 +87,9 @@ function SelectContent({
 					)}
 					{...props}
 				>
+					<SelectScrollUpButton />
 					<SelectPrimitive.List>{children}</SelectPrimitive.List>
+					<SelectScrollDownButton />
 				</SelectPrimitive.Popup>
 			</SelectPrimitive.Positioner>
 		</SelectPrimitive.Portal>
